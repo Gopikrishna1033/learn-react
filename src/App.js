69a417829ba75b7454bbae650fe1921ab17e3b17
Gopikrishna1033 <venkatagopikrishna007@gmail.com>
2024-08-09 18:52:21 +0530

    task

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ let App = ()=>{
 export default App */
 
 import React from "react";
-import User from "./User";
+import Message from "./Message";
 class App extends React.Component{
 
 render(){
 return <div>
     <h1>App Component</h1>
     <hr />
-     <User/>
+    <Message/>
 </div>
 }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+class Message extends React.Component{
+   /*  msg = "Hello"
+    clickHandler=()=>{
+        this.msg = "Gopi i love you"
+        console.log("Gopi i love you")
+        this.forceUpdate()
+    }
+    clickmeHandler=()=>{
+        this.msg = "Let's get Married"
+        console.log("Let's get Married")
+        this.forceUpdate()
+    } */
+
+    state={
+        msg : "Hello"
+    }
+    clickHandler=()=>{
+        this.setState({msg:"I Love you so Much"})
+    }
+    clickmeHandler=()=>{
+        this.setState({msg:"wiil You marry me"})
+    }
+    msg=()=>{
+        this.setState({msg:"Hello"})
+    }
+    render(){
+            return <Fragment>
+                <h2>Message Component</h2>
+                <p><b>Wish:</b>{this.state.msg}</p>
+                <button onClick={this.clickHandler
+
+                }>Click</button>
+                <button onClick={this.clickmeHandler}>Click Me</button>
+                <button onClick={this.msg}>Click Me</button>
+            </Fragment>
+    }
+}
+export default Message
